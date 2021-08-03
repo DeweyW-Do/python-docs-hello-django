b9dd16eec07eb60bff65f48398909db6bbed3473
DeweyW-Do <88354420+DeweyW-Do@users.noreply.github.com>
2021-08-03 13:58:33 +0900

    Update urls.py

--- hello/urls.py
+++ hello/urls.py
@@ -1,7 +1,17 @@
-from django.urls import path
 
+from django.urls import path, include
+from django.conf.urls import url
 from . import views
-
-urlpatterns = [    
-    path('', views.hello, name='hello'),
+ 
+urlpatterns = [   
+    url(r'^$', views.index, name='index'),
 ]
+
+###########################################
+# from django.urls import path
+
+# from . import views
+
+# urlpatterns = [    
+#     path('', views.hello, name='hello'),
+# ]
